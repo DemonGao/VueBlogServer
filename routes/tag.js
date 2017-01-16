8a5be7708af5246fa2960cb91e81e3f388a46b9c
DemonGao <a750229099@qq.com>
2017-01-16 23:44:22 +0800

    添加标签(Tag)删除方法

--- routes/tag.js
+++ routes/tag.js
@@ -5,22 +5,49 @@ var mongoose = require('mongoose')
 var Tag = mongoose.model('Tag')
 const fn = () => {}
 
+//获取全部标签
 router.get('/api/getTags', (req, res) => {
-  Tag.find({}, (err, doc) => {
-    if (err) {
-      console.log(err)
-      res.send({status:false,msg:'服务器和您开了个小小的玩笑!'})
-    } else if (doc) {
-      if(doc.length==0){
-        res.send({status:false,msg:'没有查到数据...'})
-        return ;
+  Tag
+    .find({})
+    .exec(err, doc) => {
+      if (err) {
+        console.log(err)
+        res.send({status:false,msg:'服务器和您开了个小小的玩笑!'})
+      } else if (doc) {
+        if(doc.length==0){
+          res.send({status:false,msg:'没有查到数据...'})
+          return ;
+        }
+        res.send({status:true,data:doc})
       }
-      res.send({status:true,data:doc})
-    }
+  })
+})
+/*
+router.get('/api/getTags', (req, res) => {
+  const CURRENTPAGE = req.query.currentPage;
+  const PAGESIZE = req.query.pageSize;
+  var skip_num = (CURRENTPAGE-1)*PAGESIZE;
+  Tag
+    .find({})
+    .skip(skip_num)
+    .limit(PAGESIZE)
+    .exec(err, doc) => {
+      if (err) {
+        console.log(err)
+        res.send({status:false,msg:'服务器和您开了个小小的玩笑!'})
+      } else if (doc) {
+        if(doc.length==0){
+          res.send({status:false,msg:'没有查到数据...'})
+          return ;
+        }
+        res.send({status:true,data:doc})
+      }
   })
 })
 
+*/
 
+//添加或修改标签
 router.get('/api/saveTag', (req, res) => {
   const id = req.query._id;
   const tag = {
@@ -40,7 +67,25 @@ router.get('/api/saveTag', (req, res) => {
       }
     });
   }
-  // res.status(200).end()
+})
+//删除标签
+router.post('/api/delTag',(req,res)=>{
+  const _id = req.body._id;
+  Tag.count({_id},(err,count)=>{
+    if(count!=0){
+      Tag.remove({_id},(err,result)=>{
+        if(err){
+          console.log(err);
+          res.status(200).send({status:false,msg:'删除失败!'})
+        }else{
+          console.log('标签删除成功!');
+          res.status(200).send({status:true,msg:'标签已删除!'})
+        }
+      })
+    }else{
+       res.status(200).send({status:false,msg:'标签已被删除!'})
+    }
+  })
 })
 
 module.exports = router
