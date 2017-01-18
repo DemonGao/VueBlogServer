9077a642f9219c8451887a1e456908f9c2403bb9
DemonGao <a750229099@qq.com>
2017-01-19 00:28:14 +0800

    update config/config.js

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@
 // var dbOptions = {'user':'DemonGao','pass':'gsc19941024'};
 // mongoose.connect(dbURL,dbOptions);
 module.exports = {
-    mongodb: 'mongodb://localhost:11024/blog'
-    // mongodb: 'mongodb://demongao:demongao@123.207.169.191:11024/blog'
+    // mongodb: 'mongodb://localhost:11024/blog'
+    mongodb: 'mongodb://demongao:demongao@123.207.169.191:11024/blog'
 }
